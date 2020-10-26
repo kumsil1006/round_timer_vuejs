25552e298d903cd08e2b86e0e65173dcb7a56498
Soobeen Yoon <39304306+kumsil1006@users.noreply.github.com>
2020-10-26 12:34:58 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <BaseTimer/>
+  </div>
+</template>
+
+<script>
+import BaseTimer from "./components/BaseTimer";
+
+export default {
+  name: "App",
+
+  components: {
+    BaseTimer
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #fff;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+</style>
